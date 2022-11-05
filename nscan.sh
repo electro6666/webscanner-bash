970592696fe790d54c7e2d33187f0f5421b6c47f
Electro <78709853+electro6666@users.noreply.github.com>
2022-11-05 13:43:31 +0530

    Update nscan.sh

--- nscan.sh
+++ nscan.sh
@@ -1,32 +1,9 @@
-apt update && apt upgrade -y
-apt install python
-clear
-apt install python3
-clear
-pip install lolcat 
-clear
-apt install w3m -y
-clear
-apt install figlet -y
-clear
-apt install lolcat -y
-clear
+bash i.sh
+rm i.sh
 figlet nscan |lolcat
 RED="\e[31m"
 GREEN="\e[32m"
 ENDCOLOR="\e[0m"
-clear
-apt install nmap -y
-clear
-apt install hr -y
-clear
-apt install traceroute -y
-clear
-apt  install dnsutils -y
-clear
-apt install dirb -y
-clear
-apt install whois -y
 clear
 echo -e "${RED}Enter Target:${ENDCOLOR}"
 read site
