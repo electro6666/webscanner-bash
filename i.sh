a7ee57a20e6fb86b5f84b268ac2017f6b1f7c55b
Electro <78709853+electro6666@users.noreply.github.com>
2022-11-05 13:43:12 +0530

    Create i.sh

---
+++ i.sh
@@ -0,0 +1,25 @@
+apt update && apt upgrade -y
+apt install python
+clear
+apt install python3
+clear
+pip install lolcat 
+clear
+apt install w3m -y
+clear
+apt install figlet -y
+clear
+apt install lolcat -y
+clear
+apt install nmap -y
+clear
+apt install hr -y
+clear
+apt install traceroute -y
+clear
+apt  install dnsutils -y
+clear
+apt install dirb -y
+clear
+apt install whois -y
+clear
