1e971ce0ac16f239bc2f80bffe1525fd3c98c8a2
Electro <78709853+electro6666@users.noreply.github.com>
2023-11-14 07:09:11 +0530

    Update nscan.sh

--- nscan.sh
+++ nscan.sh
@@ -34,4 +34,59 @@ sleep 1
 nmap -v  --script http-sitemap-generator.nse $site
 echo 'wait........' 
 sleep 4
+echo -e "${GREEN}________________________Vulnerability Scanning Script________________________${ENDCOLOR}"
+
+# Heartbleed vulnerability scan
+nmap -p 443 --script ssl-heartbleed $site
+
+# Shellshock vulnerability scan
+nmap -p 80 --script http-shellshock $site
+
+# POODLE vulnerability scan
+nmap --script ssl-poodle -p 443 $site
+
+# SMB vulnerabilities scan
+nmap -p 445 --script smb-vuln* $site
+
+# DNS vulnerabilities scan
+nmap --script dns-vuln* $site
+
+# SNMP vulnerabilities scan
+nmap -p 161,162 --script snmp* $site
+
+# FTP vulnerabilities scan
+nmap -p 21 --script ftp* $site
+
+# Additional features section
+echo -e "${GREEN}________________________Additional Vulnerability Features________________________${ENDCOLOR}"
+
+# Web Application Vulnerabilities scan
+nmap -p 80,443 --script http-vuln* $site
+
+# MySQL Vulnerabilities scan
+nmap -p 3306 --script mysql-vuln* $site
+
+# SNMP Enumeration scan
+nmap -p 161,162 --script snmp-enum $site
+
+# NFS Vulnerabilities scan
+nmap -p 2049 --script nfs* $site
+
+# MSRPC Vulnerabilities scan
+nmap -p 135 --script msrpc* $site
+
+# SMTP Vulnerabilities scan
+nmap -p 25 --script smtp* $site
+
+# PostgreSQL Vulnerabilities scan
+nmap -p 5432 --script postgresql* $site
+
+# Additional vulnerabilities
+nmap -p 3389 --script rdp-vuln* $site   # RDP vulnerabilities
+nmap -p 53 --script dns-recursion $site   # DNS Recursion check
+nmap -p 389 --script ldap* $site   # LDAP vulnerabilities
+
+# Final banner
+echo -e "${GREEN}________________________Scan Completed________________________${ENDCOLOR}"
+
 dirb http://$site -f
